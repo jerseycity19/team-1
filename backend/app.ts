9db252d5cafc45b0bdd8ea6bd4c1828b35070577
davidnso <davidnsoesie1@gmail.com>
2019-09-27 19:19:12 -0400

    build the backend server on port 1000 (local env)

--- backend/app.ts
+++ backend/app.ts
@@ -1,1 +1,13 @@
-console.log("We are up and running");
+import { ExpressDriver } from "./src/drivers/express/expressDriver";
+import * as http from "http";
+import { MongoDriver } from "./src/drivers";
+require("dotenv").config();
+
+const app = ExpressDriver.build();
+/**
+ * build the express driver and start the application server.
+ */
+const server = http.createServer(app);
+server.listen(process.env.PORT, () => {
+  console.log("Scholars at risk API running on " + process.env.PORT);
+});
