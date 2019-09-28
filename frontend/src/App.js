7397f7277184f92bd22a4673d82e979ed8680a74
Paschal Igusti <paschali@bu.edu>
2019-09-28 10:39:52 -0400

    Login button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -36,8 +36,11 @@ class TopNavBar extends React.Component {
           <Col sm={{ span: 4 }}>
             <img src={logo} className="imageStyles"></img>
           </Col>
-          <Col sm={{ span: 8}} className="NavbarStyles">
+          <Col sm={{ span: 6}} className="NavbarStyles">
             Protecting scholars and the freedom to think, question, and share ideas
+          </Col>
+          <Col sm={{ span: 1}} className="NavbarStyles">
+            <NavLink to="/login">Temp</NavLink>
           </Col>
         </Row>
       </Container>
