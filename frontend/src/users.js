98909bc75c42606756df0787a0f02b113007b937
Paschal Igusti <paschali@bu.edu>
2019-09-27 23:17:53 -0400

    Question funcionality

--- frontend/src/users.js
+++ frontend/src/users.js
@@ -5,26 +5,51 @@ import axios from 'axios';
 const User = ({ match }) => <p>{match.params.id}</p>;
 
 class Users extends React.Component {
-    state = {
-      message: "temp"
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        QuestionNumber: 0,
+        questions: [],
+        questionLength: 0
+      }
+
+      this.questionController = this.questionController.bind(this);
     }
 
     componentDidMount() {
-      axios.get('http://localhost:1000/')
+      axios.get('http://localhost:1000/api/questions')
         .then(res => {
-          const message1 = res.data.message;
+          //const message1 = res.data.message;
           console.log(res);
-          this.setState({message: message1})
+          //this.setState({message: message1})
+          let questionArray = [];
+          for (let i = 0; i < res.data.length; i++) {
+            questionArray.push(res.data[i].text);
+          }
+          this.setState({questions: questionArray, questionLength: questionArray.length});
+
         })
+      this.setState({questionLength: this.state.questions.length});
     }
 
+    questionController() {
+      if (this.state.QuestionNumber < this.state.questionLength - 1) {
+        this.setState({QuestionNumber: this.state.QuestionNumber += 1})
+      }
+      else {
+        alert("MAX NUM QUESTIONS REACHED");
+      }
+    }
+
+
+
     render() {
-    console.log("test");
+    console.log(this.state.QuestionNumber);
+    let index = this.state.QuestionNumber;
     return (
         <div>
-            <h1>Users</h1>
-          <strong>select a user</strong>
-          <ul>
+          {/* <ul>
             <li>
               <Link to="/users/1">User 1 </Link>
             </li>
@@ -34,8 +59,10 @@ class Users extends React.Component {
             <li>
               <Link to="/users/3">User 3 </Link>
             </li>
-          </ul>
+          </ul> */}
+          <button onClick={this.questionController}>Next Question</button>
           <p>{this.state.message}</p>
+          <p>{this.state.questions[index]}</p>
           <Route path="/users/:id" component={User} />
         </div>
 
