46a3b56b92d4177a1775bf39bc936252a61cbb53
Paschal Igusti <paschali@bu.edu>
2019-09-28 09:28:46 -0400

    Progress bar and POST body manipulation

--- frontend/src/screens/AdminPanel.js
+++ frontend/src/screens/AdminPanel.js
@@ -11,15 +11,32 @@ class AdminPanel extends React.Component {
       this.state = {
         totalNumberSubmissions: 100,
         topThreeCountries: ['Turkey', 'Syria', 'Iran'],
-        reccuringPings: ["199.253.244.19", "199.233.214.19", "199.213.234.29"]
+        reccuringPings: ["199.253.244.19", "199.233.214.19", "199.213.234.29"],
+        killings: "",
+        imprisonment: "",
+        prosecution: "",
+        lossOfPosition: "",
+        travelRestrictions: "",
+        other: ""       
       }
-
-
     };
 
 
+
+
     componentDidMount() {
-        this.interval = setInterval(() => this.setState({ totalNumberSubmissions: this.state.totalNumberSubmissions += this.state.totalNumberSubmissions % 3 }), 1000);
+        axios.get("http://localhost:1000/api/metrics")
+            .then(res => {
+                this.setState({
+                    killings: res.data.killings,
+                    imprisonment: res.data.imprisonment,
+                    prosecution: res.data.prosecution,
+                    lossOfPosition: res.data.lossOfPosition,
+                    travelRestrictions: res.data.travelRestrictions,
+                    other: res.data.other
+                })
+            })
+        this.interval = setInterval(() => this.setState({ totalNumberSubmissions: this.state.totalNumberSubmissions += this.state.totalNumberSubmissions % 3 }), 30000);
     }
     componentWillUnmount() {
     clearInterval(this.interval);
@@ -36,6 +53,12 @@ class AdminPanel extends React.Component {
                 <p>Total number of submissions: {this.state.totalNumberSubmissions}</p>
                 <p>Current Top Three Countries with SAR: {this.state.topThreeCountries[0]}, {this.state.topThreeCountries[1]}, {this.state.topThreeCountries[2]}</p>
                 <p>Recurring IPs (Potential Fakes): {this.state.reccuringPings[0]}, {this.state.reccuringPings[1]}, {this.state.reccuringPings[2]}</p>
+                <p>Killings: {this.state.killings}</p>
+                <p>Imprisonment: {this.state.imprisonment}</p>
+                <p>Prosecution: {this.state.prosecution}</p>
+                <p>Loss of Position: {this.state.lossOfPosition}</p>
+                <p>Travel Restrictions: {this.state.travelRestrictions}</p>
+                <p>Other: {this.state.other}</p>
             </div>
         )
 
