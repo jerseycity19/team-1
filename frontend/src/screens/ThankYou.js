f0dcb6ec5b3bf123cea3df7cda9e8df248753611
Paschal Igusti <paschali@bu.edu>
2019-09-28 10:16:10 -0400

    ty page

---
+++ frontend/src/screens/ThankYou.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Select from "react-select";
+import { Route, Link, NavLink} from "react-router-dom";
+import axios from 'axios';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import '../styles/isa-index.css';
+import frame from '../images/Frame.png';
+
+class ImageFrame extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={frame} className="imageFrame"></img>
+      </div>
+    );
+  }
+}
+
+class ThankYou extends React.Component {
+    render() {
+
+        return (
+        <div>
+            <h1>Thank you for sharing your views.</h1>
+            <NavLink to="/"><button className="lastButton" onClick={this.questionController}>Home</button></NavLink>
+            <ImageFrame />
+        </div>
+
+    );
+  }
+
+}
+
+export default ThankYou;
