19464567aaff336d014e2cf68c8d7df50fb17ae0
Paschal Igusti <paschali@bu.edu>
2019-09-28 08:18:16 -0400

    Mock MVP

--- frontend/src/screens/AdminPanel.js
+++ frontend/src/screens/AdminPanel.js
@@ -10,7 +10,8 @@ class AdminPanel extends React.Component {
 
       this.state = {
         totalNumberSubmissions: 100,
-        topThreeCountries: ['Turkey', 'Syria', 'Iran']
+        topThreeCountries: ['Turkey', 'Syria', 'Iran'],
+        reccuringPings: ["199.253.244.19", "199.233.214.19", "199.213.234.29"]
       }
 
 
@@ -34,6 +35,7 @@ class AdminPanel extends React.Component {
             <div>
                 <p>Total number of submissions: {this.state.totalNumberSubmissions}</p>
                 <p>Current Top Three Countries with SAR: {this.state.topThreeCountries[0]}, {this.state.topThreeCountries[1]}, {this.state.topThreeCountries[2]}</p>
+                <p>Recurring IPs (Potential Fakes): {this.state.reccuringPings[0]}, {this.state.reccuringPings[1]}, {this.state.reccuringPings[2]}</p>
             </div>
         )
 
